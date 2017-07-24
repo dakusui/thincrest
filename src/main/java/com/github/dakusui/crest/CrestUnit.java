c7c32266a0773f76fadff47bb8be94fe5aefdc40
Hiroshi Ukai <dakusui@gmail.com>
2017-07-25 07:09:25 +0900

    Implement basic functionality. (wip)

--- src/main/java/com/github/dakusui/crest/CrestUnit.java
+++ src/main/java/com/github/dakusui/crest/CrestUnit.java
@@ -3,7 +3,7 @@ package com.github.dakusui.crest;
 import org.junit.After;
 import org.junit.Before;
 
-public abstract class CrestBase {
+public abstract class CrestUnit {
   @Before
   public void before() {
     CrestUtils.suppressStdOutErrIfRunUnderSurefire();
--- src/main/java/com/github/dakusui/crest/Formattable.java
+++ src/main/java/com/github/dakusui/crest/Formattable.java
@@ -7,13 +7,43 @@ import java.util.function.Predicate;
 public enum Formattable {
   ;
 
-  public static <I, O> Function<I, O> function(String s, Function<I, O> function) {
+  public static <T, R> Function<T, R> function(String s, Function<T, R> function) {
     Objects.requireNonNull(s);
     Objects.requireNonNull(function);
-    return new Function<I, O>() {
+    return new Function<T, R>() {
       @Override
-      public O apply(I i) {
-        return function.apply(i);
+      public R apply(T t) {
+        return function.apply(t);
+      }
+
+      public <V> Function<V, R> compose(Function<? super V, ? extends T> before) {
+        Objects.requireNonNull(before);
+        return new Function<V, R>() {
+          @Override
+          public R apply(V v) {
+            return function.apply(before.apply(v));
+          }
+
+          @Override
+          public String toString() {
+            return String.format("%s->%s", before, function);
+          }
+        };
+      }
+
+      public <V> Function<T, V> andThen(Function<? super R, ? extends V> after) {
+        Objects.requireNonNull(after);
+        return new Function<T, V>() {
+          @Override
+          public V apply(T t) {
+            return after.apply(function.apply(t));
+          }
+
+          @Override
+          public String toString() {
+            return String.format("%s->%s", function, after);
+          }
+        };
       }
 
       @Override
@@ -23,11 +53,58 @@ public enum Formattable {
     };
   }
 
-  public static <I> Predicate<I> predicate(String s, Predicate<I> predicate) {
-    return new Predicate<I>() {
+  public static <T> Predicate<T> predicate(String s, Predicate<T> predicate) {
+    return new Predicate<T>() {
       @Override
-      public boolean test(I i) {
-        return predicate.test(i);
+      public boolean test(T t) {
+        return predicate.test(t);
+      }
+
+      @Override
+      public Predicate<T> and(Predicate<? super T> other) {
+        Objects.requireNonNull(other);
+        return new Predicate<T>() {
+          @Override
+          public boolean test(T t) {
+            return predicate.test(t) && other.test(t);
+          }
+
+          @Override
+          public String toString() {
+            return String.format("%s&&%s", predicate, other);
+          }
+        };
+      }
+
+      @Override
+      public Predicate<T> negate() {
+        return new Predicate<T>() {
+          @Override
+          public boolean test(T t) {
+            return !predicate.test(t);
+          }
+
+          @Override
+          public String toString() {
+            return String.format("!%s", predicate);
+          }
+        };
+      }
+
+      @Override
+      public Predicate<T> or(Predicate<? super T> other) {
+        Objects.requireNonNull(other);
+        return new Predicate<T>() {
+          @Override
+          public boolean test(T t) {
+            return predicate.test(t) || other.test(t);
+          }
+
+          @Override
+          public String toString() {
+            return String.format("(%s||%s)", predicate, other);
+          }
+        };
       }
 
       @Override
@@ -35,9 +112,5 @@ public enum Formattable {
         return s;
       }
     };
-  }
-
-  public static void main(String... args) {
-    System.out.println(function("Hello(x)", x -> "Hello:" + x));
   }
 }
