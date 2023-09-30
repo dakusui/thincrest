d893d12916a1ee45dc1816dd9195f8cf4aa7492e
Hiroshi Ukai <dakusui@gmail.com>
2023-09-30 16:51:21 +0900

    pom.xml refactoring.

--- src/main/build_tools/package-info-adoc_to_package-info-java.sh
+++ src/main/build_tools/package-info-adoc_to_package-info-java.sh
@@ -11,6 +11,9 @@ function convert_package_info_adoc_to_package_info_java() {
   echo "/**" >> ${_out}
   while IFS= read -r _i
   do
+    _i="${_i//>/&gt;}"
+    _i="${_i//</&lt;}"
+    _i="${_i//&/&amp;}"
     echo " * ${_i}" >> ${_out}
   done < <(cat "${_base_dir}/${_rel_dir}/package-info.adoc" <(echo ""))
 
